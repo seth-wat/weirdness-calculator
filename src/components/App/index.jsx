121436c22f1ba94ff03c529a091e4ee4c16c2a30
Seth Watkins <dontpanic@Seths-MacBook-Pro.local>
2019-05-08 10:47:47 -0500

    feat: add intro para component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Header } from "../Header";
 import { Column } from "../Column";
+import { IntroParagraphs } from "../IntroParagraphs";
+import { Row } from "../Row";
 
 export const App = props => {
   return (
     <Column styleOverrides={{ height: "100vh" }}>
       <Header />
+      <Column>
+        <IntroParagraphs />
+      </Column>
     </Column>
   );
 };
---
+++ src/components/IntroParagraphs/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+
+export const IntroParagraphs = props => {
+  return (
+    <React.Fragment>
+      <p className="bp3-ui-text">
+        Find out how weird you are by selecting the GIFs that make you laugh.
+        We’ll show you the least weird ones to start, but you can move the
+        slider to make them weirder.
+      </p>
+      <br />
+      <p className="bp3-ui-text">
+        When you find a GIF you like, press the <em>Like button</em>. Once you
+        like 5 GIFs, we’ll show you how weird you are.
+      </p>
+    </React.Fragment>
+  );
+};
