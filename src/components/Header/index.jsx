4fb64c89cf6c79213e2516ef514a62eeefb9dfc6
Seth Watkins <dontpanic@Seths-MacBook-Pro.local>
2019-05-08 10:31:55 -0500

    chore: add blueprint stylesheets, style header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import { Row } from "../Row";
 
 export const Header = props => (
   <Row>
-    <h1 className="bp3-heading">Weirdness Calculator</h1>
+    <h1 className="bp3-heading" style={{ padding: "1rem" }}>
+      Weirdness Calculator
+    </h1>
   </Row>
 );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { App } from "./components/App";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
