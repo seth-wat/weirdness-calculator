cde29dc66d29d861d69acf2b6bf249f956e9c49e
Seth Watkins <dontpanic@Seths-MacBook-Pro.local>
2019-05-10 14:24:36 -0500

    feat: add react-router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,64 +1,11 @@
 import React from "react";
-import { Column } from "../Column";
-import { Row } from "../Row";
-import { Header } from "../Header";
-import { SearchSection } from "../SearchSection";
-import { ResultSection } from "../ResultSection";
-import { FavoritesSection } from "../FavoritesSection";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { CalculatorRoute } from "../CalculatorRoute";
 
 export const App = () => {
   return (
-    <Column
-      styleOverrides={{
-        height: "100vh",
-        minHeight: "100vh",
-        overflow: "hidden"
-      }}
-    >
-      <Row styleOverrides={{ maxHeight: "10%", minHeight: "10%" }}>
-        <Header />
-      </Row>
-      <Row styleOverrides={{ maxHeight: "90%", minHeight: "90%" }}>
-        <Column
-          styleOverrides={{
-            width: "60%",
-            minHeight: "100%",
-            maxHeight: "100%"
-          }}
-        >
-          <Column
-            styleOverrides={{
-              minHeight: "40%",
-              maxHeight: "40%",
-              justifyContent: "center"
-            }}
-          >
-            <SearchSection />
-          </Column>
-          <Column
-            styleOverrides={{
-              backgroundColor: "#FFCC80",
-              minHeight: "60%",
-              maxHeight: "60%",
-              justifyContent: "center",
-              borderTop: "1px solid #FFA726"
-            }}
-          >
-            <ResultSection />
-          </Column>
-        </Column>
-        <Column
-          styleOverrides={{
-            width: "40%",
-            minHeight: "100%",
-            maxHeight: "100%",
-            backgroundColor: "#FFCC80",
-            borderLeft: "1px solid #FFA726"
-          }}
-        >
-          <FavoritesSection />
-        </Column>
-      </Row>
-    </Column>
+    <Router>
+      <Route path="/" exact component={CalculatorRoute} />
+    </Router>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./components/App";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { App } from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
